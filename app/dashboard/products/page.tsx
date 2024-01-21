ec0dd2e693e9a88ff8a9c71e27ff2a956aee6133
Pavlo Shabak <properfraction66@yahoo.com>
2024-01-21 11:44:46 +0200

    Create layouts for Users, Products components; create Pagination and Search components

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,102 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Search from '@/app/ui/dashboard/Search/Search';
+import Pagination from '@/app/ui/dashboard/Pagination/Pagination';
+import styles from '@/app/ui/dashboard/Products/Products.module.css';
+
 const ProductsPage = () => {
-  return <div>Products</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder='Search for a product...' />
+        <Link href='/dashboard/products/add'>
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || '/noproduct.jpg'}
+                    alt=''
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.desc}</td>
+              <td>${product.price}</td>
+              <td>{product.createdAt?.toString().slice(4, 16)}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteProduct}>
+                    <input type='hidden' name='id' value={product.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))} */}
+          <td>
+            <div className={styles.product}>
+              <Image
+                src={'/noproduct.jpg'}
+                alt=''
+                width={40}
+                height={40}
+                className={styles.productImage}
+              />
+              Iphone 12
+            </div>
+          </td>
+          <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</td>
+          <td>$1000</td>
+          <td>12.12.2023</td>
+          <td>In stock</td>
+          <td>
+            <div className={styles.buttons}>
+              <Link href={`/dashboard/products`}>
+                <button className={`${styles.button} ${styles.view}`}>
+                  View
+                </button>
+              </Link>
+              <form>
+                <input type='hidden' name='id' />
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </form>
+            </div>
+          </td>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,5 +1,102 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Pagination from '@/app/ui/dashboard/Pagination/Pagination';
+import Search from '@/app/ui/dashboard/Search/Search';
+import styles from '@/app/ui/dashboard/Users/Users.module.css';
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder='Search for a user...' />
+        <Link href='/dashboard/users/add'>
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || '/noavatar.png'}
+                    alt=''
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? 'Admin' : 'Client'}</td>
+              <td>{user.isActive ? 'active' : 'passive'}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteUser}>
+                    <input type='hidden' name='id' value={user.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))} */}
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src={'/noavatar.png'}
+                  alt=''
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Jhon Doe
+              </div>
+            </td>
+            <td>jhon.doe@gmail.com</td>
+            <td>12.12.2023</td>
+            <td>Client</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <button className={`${styles.button} ${styles.view}`}>
+                  View
+                </button>
+                <form>
+                  <input type='hidden' name='id' />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default UsersPage;
