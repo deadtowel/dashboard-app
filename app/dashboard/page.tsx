b30768dfbabf8dc7e36ce49be895c84d53ff295e
Pavlo Shabak <properfraction66@yahoo.com>
2024-01-21 10:18:42 +0200

    Create Card component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,32 @@
+import Card from '@/app/ui/dashboard/Card/Card';
+import Transactions from '@/app/ui/dashboard/Transactions/Transactions';
+import Chart from '@/app/ui/dashboard/Chart/Chart';
+import Rightbar from '@/app/ui/dashboard/Rightbar/Rightbar';
 
+import styles from '@/app/ui/dashboard/dashboard.module.css';
 
-type DashboardPageProps = {}
+type DashboardPageProps = {};
 
 const DashboardPage: React.FC<DashboardPageProps> = () => {
   return (
-    <div>pDashboard</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {/* {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))} */}
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
 
 export default DashboardPage;
---
+++ app/ui/dashboard/Card/Card.tsx
@@ -0,0 +1,28 @@
+import { MdSupervisedUserCircle } from 'react-icons/md';
+import styles from './Card.module.css';
+
+const item = {
+  title: 'Sales',
+  number: '$10327',
+  change: 12.54,
+};
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>{item.title}</span>
+        <span className={styles.number}>{item.number}</span>
+        <span className={styles.detail}>
+          <span className={item.change > 0 ? styles.positive : styles.negative}>
+            {item.change}%
+          </span>{' '}
+          {item.change > 0 ? 'more' : 'less'} than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
